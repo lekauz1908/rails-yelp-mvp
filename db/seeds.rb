de3194bfbe479d90c249055842439c7eb9692b3a
marc q <marc.quitschau@gmail.com>
2020-05-20 17:00:05 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#puts "Cleaning database..."
-#Restaurants.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5, category: "chinese" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4 , category: "italian"}
-poke_bowl = { name: "POKEBOWL", address: "Stg-West", rating: 3, category: "japanese" }
-fries_for_all =  { name: "Fries For All", address: "FRA, Airport", rating: 2 , category: "belgian"}
-confit = { name: "Confit", address: "Paris", rating: 5, category: "french" }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+poke_bowl = { name: "POKEBOWL", address: "Stg-West", category: "japanese" }
+fries_for_all =  { name: "Fries For All", address: "FRA, Airport", category: "belgian"}
+confit = { name: "Confit", address: "Paris", category: "french" }
 
-#[dishoom, pizza_east, poke_bowl, fries_for_all, confit].each do |attributes|
- # restaurant = Restaurant.create!(attributes)
-  #puts "Created #{restaurant.name}"
-#end
-#puts "Finished" 
+[dishoom, pizza_east, poke_bowl, fries_for_all, confit].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished" 
 
